6a9a7f0048a43490fcff8bf30a6e4ca580acec4a
Kazem Qulian <kgholian@gamil.com>
2023-08-20 19:28:58 +0330

    global css and main font added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
-import "./App.css"
+import { styled } from "styled-components";
+import "./App.css";
+
+const Container = styled.div`
+  width: calc(100% - 220px);
+  max-width: 1060px;
+  margin: 70px auto;
+  display: flex;
+  flex-flow: column nowrap;
+  gap: 16px;
+`;
 
 function App() {
-  return <></>;
+  return <Container><h1>Task List</h1></Container>;
 }
 
 export default App;
